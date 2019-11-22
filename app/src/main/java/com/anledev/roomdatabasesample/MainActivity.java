307def84eb21ff36f9171e27fb43db57f4bb3de0
anlt <anlt@rikkeisoft.com>
2019-11-22 13:50:35 +0700

    Part 5 – ViewModel

--- app/src/main/java/com/anledev/roomdatabasesample/MainActivity.java
+++ app/src/main/java/com/anledev/roomdatabasesample/MainActivity.java
@@ -1,14 +1,30 @@
 package com.anledev.roomdatabasesample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
